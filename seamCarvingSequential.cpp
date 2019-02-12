c4ee618d427150b54377ed29e4dc25dfcc543f1d
Scott Roberts <scottroberts@Scotts-MacBook-Pro.local>
2019-02-12 18:19:19 -0500

    WIP: rectangle bug

--- seamCarvingSequential.cpp
+++ seamCarvingSequential.cpp
@@ -31,6 +31,7 @@ void write_pgm(vector<vector<int> > greyscale){
     int i, j, temp = 0; 
     int width = static_cast<int>(greyscale.size());
     int height = static_cast<int>(greyscale[0].size());
+
   
     FILE* pgmimg; 
     pgmimg = fopen("pgmimg.pgm", "wb"); 
@@ -39,14 +40,14 @@ void write_pgm(vector<vector<int> > greyscale){
     fprintf(pgmimg, "P2\n");  
   
     // Writing Width and Height 
-    fprintf(pgmimg, "%d %d\n", width, height);  
+    fprintf(pgmimg, "%d %d \n", width, height);  
   
     // Writing the maximum gray value 
-    fprintf(pgmimg, "255");  
+    fprintf(pgmimg, " 255");  
     int count = 0; 
     for (i = 0; i < height; i++) { 
         for (j = 0; j < width; j++) { 
-            temp = (unsigned char)greyscale[i][j];
+            temp = greyscale[i][j];
   
             // Writing the gray values in the 2D array to the file 
             fprintf(pgmimg, "%d\n", temp); 
@@ -58,14 +59,14 @@ void write_pgm(vector<vector<int> > greyscale){
 
 vector<vector<int> > energy_data(CImg<unsigned char> image){
   
-  vector<vector<int> > results;
   int row, col, diffx, diffy, diffxy, width, height;
   int tempPixel;
   width = static_cast<int>(image.width());
   height = static_cast<int>(image.height());
-  int holder[width][height];
+
+  vector<vector<int> > results(width, vector<int>(height));
+  
   for(row = 0;row<width; row ++) {
-    vector<int> row_vec;
     for(col =0; col<height; col++){
       //cout <<"pixel is " <<static_cast<int>(image.atXY(row,col))<<"\n";
       diffx = abs(image.atXY(col, row) - image.atXY(col, row+1));
@@ -76,10 +77,10 @@ vector<vector<int> > energy_data(CImg<unsigned char> image){
       if (tempPixel>255)
         tempPixel =255;
 
-      holder[row][col] = tempPixel;
-      row_vec.push_back(holder[row][col]);
+      results[row][col] = tempPixel;
+      //row_vec.push_back(holder[row][col]);
     }
-    results.push_back(row_vec);
+    //results.push_back(row_vec);
   }
 
   return results;
@@ -87,24 +88,19 @@ vector<vector<int> > energy_data(CImg<unsigned char> image){
 
 vector<vector<int> > writeIntoVector(CImg<unsigned char> image){
   
-  vector<vector<int> > results;
   int width, height, row, col;
   int tempPixel;
   width = static_cast<int>(image.width());
   height = static_cast<int>(image.height());
-  int holder[width][height];
+  vector<vector<int> > results(width, vector<int>(height));
   for(row = 0;row<width; row ++) {
-    vector<int> row_vec;
     for(col =0; col<height; col++){
       tempPixel = image.atXY(col,row);
-
       if (tempPixel>255)
         tempPixel =255;
 
-      holder[row][col] = tempPixel;
-      row_vec.push_back(holder[row][col]);
+      results[row][col] = tempPixel;
     }
-    results.push_back(row_vec);
   }
 
   return results;
@@ -114,11 +110,13 @@ vector<vector<int> > writeIntoVector(CImg<unsigned char> image){
 int main() {
   CImg<unsigned char> bwImage("testLandscape.pgm");
   CImg<unsigned char> image("test.pgm");
-  cout <<"cImg size" << static_cast<int>(bwImage.size());
   vector<vector<int> > test = energy_data(bwImage);
+  vector<vector<int> > test2 = writeIntoVector(bwImage);
   write_pgm(test);
+
   CImg<unsigned char>output("pgmimg.pgm");
-  CImgDisplay main_disp(bwImage, "Testing"), main_display_output(output,"See anything?");
+
+  CImgDisplay main_disp(output,"energies"), other_dip(bwImage, "original");
   while (!main_disp.is_closed()) {
     main_disp.wait();
     }
