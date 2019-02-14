17f2c6e9872c1e53e763be6efe7d89213ad229c7
Scott Roberts <scottroberts@Scotts-MacBook-Pro.local>
2019-02-13 22:46:59 -0500

    cumulativeEnergy calculated

--- seamCarvingSequential.cpp
+++ seamCarvingSequential.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-// #include <ImageMagick-7>
 
 #include <sstream>      /*string stream*/
 #include <stdlib.h>     /* srand, rand */
@@ -43,19 +42,67 @@ void write_pgm(vector<vector<int> > greyscale){
     fprintf(pgmimg, "%d %d \n", width, height);  
   
     // Writing the maximum gray value 
-    fprintf(pgmimg, " 255");  
+    fprintf(pgmimg, " 255\n");  
     int count = 0; 
-    for (i = 0; i < height; i++) { 
-        for (j = 0; j < width; j++) { 
-            temp = greyscale[i][j];
+    for (i = 0; i < width; i++) { 
+        for (j = 0; j < height; j++) {
+            temp = greyscale[j][i];
   
             // Writing the gray values in the 2D array to the file 
             fprintf(pgmimg, "%d\n", temp); 
         } 
-        fprintf(pgmimg, "\n"); 
     } 
     fclose(pgmimg); 
 } 
+
+int getLowestBelow(vector<int> below, int col){
+  int left = col-1;
+  int right = col+1;
+  if (left>=0){
+    if (right<static_cast<int>(below.size())){
+      if (below[right]<below[left]){
+        if (below[right]< below[col]){
+          return below[right];
+        } else {
+          return below[col];
+        }
+      } else{
+        if (below[left]< below[col]){
+          return below[left];
+        } else{
+          return below[col];
+        }
+      }
+    }
+  }
+  else if (right < static_cast<int>(below.size())){
+    if (below[right] < below[col]){
+      return below[right];
+    } else {
+      return below[col];
+    }
+  } else {
+    return below[col];
+  }
+
+}
+
+vector<vector<int> > accumulations_creator(vector<vector<int> > energies) {
+  int width = static_cast<int>(energies.size());
+  int height = static_cast<int>(energies[0].size());
+
+  vector<vector<int> > results(width, vector<int>(height, 0));
+  
+  for (int row = (width -2); row> -1; row --){
+    vector<int> prevRow = energies[row+1];
+    for (int col= (height-1); col>-1; col--){
+      results[row][col]= getLowestBelow(prevRow,col)+ energies[row][col];
+    }
+  }
+
+  return results;
+
+}
 
 vector<vector<int> > energy_data(CImg<unsigned char> image){
   
@@ -108,17 +155,19 @@ vector<vector<int> > writeIntoVector(CImg<unsigned char> image){
 
 
 int main() {
-  CImg<unsigned char> bwImage("testLandscape.pgm");
-  CImg<unsigned char> image("test.pgm");
+  CImg<unsigned char> bwImage("tester.pgm");
+  // CImg<unsigned char> image("test.pgm");
   vector<vector<int> > test = energy_data(bwImage);
-  vector<vector<int> > test2 = writeIntoVector(bwImage);
-  write_pgm(test);
+  printVector(test);
+  vector<vector<int> > test2 = accumulations_creator(test);
+  printVector(test2);
+  // write_pgm(test);
 
-  CImg<unsigned char>output("pgmimg.pgm");
+  // CImg<unsigned char>output("pgmimg.pgm");
 
-  CImgDisplay main_disp(output,"energies"), other_dip(bwImage, "original");
-  while (!main_disp.is_closed()) {
-    main_disp.wait();
-    }
+  // CImgDisplay main_disp(output,"energies"), other_dip(bwImage, "original");
+  // while (!main_disp.is_closed()) {
+  //   main_disp.wait();
+  //   }
   return 0;
 }
